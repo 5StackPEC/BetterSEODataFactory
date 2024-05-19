440d8be9ea7a3373043d6db273b1506c6d6c528c
CristianCazares <cm_cjavier@hotmail.com>
2024-05-19 15:26:20 -0600

    feat(main-notebook): implemented annotations draft

--- js/boundingBoxes.js
+++ js/boundingBoxes.js
@@ -1,14 +1,4 @@
-function myFunction(param1, param2) {
-  // Your function logic here
-  console.log(param1 + param2);
-}
-
-/**
- * Adds two numbers.
- * @param {string} xPos The first number.
- * @param {string} yPos The second number.
- */
-const drawPositionLabel = (posX, posY) => {
+paintCornerLabels = function (posX, posY) {
   var div = document.createElement("div");
 
   // Set the content of the div (optional)
@@ -19,21 +9,18 @@ const drawPositionLabel = (posX, posY) => {
   // Append the div to the document body since getting the size is only possible after doing so
   document.body.appendChild(div);
 
-  var centerX = posX - div.offsetWidth / 2;
-  var centerY = posY - div.offsetHeight / 2;
+  var centerX =  posX  - div.offsetWidth / 2;
+  var centerY =  posY  - div.offsetHeight / 2;
+
+  const offset_widths = [
+    div.offsetWidth,
+    div.offsetWidth / 2,
+    div.offsetHeight,
+    div.offsetHeight / 2,
+  ];
 
   // Apply CSS styles for positioning
   div.style.left = centerX + "px"; // Adjust left position as needed
   div.style.top = centerY + "px"; // Adjust top position as needed
   div.style.backgroundColor = "rgba(255, 0, 0, 0.5)"; // Example background color
-};
-
-/**
- * Adds two numbers.
- * @param {number} x The first number.
- * @param {number} y The second number.
- * @returns {number} The sum of x and y.
- */
-const drawBoundingBox = (element) => {
-  element.style.border = "blue solid 2px";
 };
---
+++ js/isInViewPort.js
@@ -0,0 +1,3 @@
+isInViewPort = function(element) {
+  var bounding = el.bounding
+}
