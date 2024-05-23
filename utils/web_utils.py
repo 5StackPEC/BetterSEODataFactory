8daf7ae7120b3a26947f8ba84732d3a40638c428
CristianCazares <cm_cjavier@hotmail.com>
2024-05-22 19:43:30 -0600

    hotfix(utils/web): updated get_site_name function

--- utils/web_utils.py
+++ utils/web_utils.py
@@ -1,26 +1,25 @@
 from urllib.parse import urlparse
 import json
 from utils import web_utils
+import tldextract
 
+# types
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-def get_site_name(url):
+def get_site_name_old(url):
     parsed_url = urlparse(url)
     domain = parsed_url.netloc
     domain_name = domain.split(".")[0]
     return domain_name
 
 
-def take_screenshot(driver: WebDriver, url):
+def get_site_name(url):
+    return tldextract.extract(url).domain
+
+
+def take_screenshot_of_window(driver: WebDriver, url):
+    # The url is only needed for the file name, it does not actually uses it!
     driver.fullscreen_window()
     driver.save_screenshot(f"./dataset/images/{web_utils.get_site_name(url)}.png")
-
-
-def load_js_script_to_driver(driver: WebDriver, js_script_path="./js/boundingBoxes.js"):
-    with open(js_script_path, "r") as file:
-        js_code = file.read()
-
-    driver.execute_script(js_code)
-
-    return driver
+    driver.maximize_window()
