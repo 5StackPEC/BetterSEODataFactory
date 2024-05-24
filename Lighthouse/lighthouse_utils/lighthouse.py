c6c3e618c0e1e23160f21fac4caa562cc9decbd5
CristianCazares <cm_cjavier@hotmail.com>
2024-05-24 15:31:37 -0600

    hotfix(Lighthouse/npm): changed from global to local packages

--- Lighthouse/lighthouse_utils/lighthouse.py
+++ Lighthouse/lighthouse_utils/lighthouse.py
@@ -1,12 +1,19 @@
 import json
 import subprocess
 
+
 def run_lighthouse(url):
-    cmd = f"lighthouse {url} --output=json --quiet"
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+    cmd = f"npx lighthouse {url} --output=json"
+    result = subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    error_message_preffix = f"Error running Lighthouse command for {url}:"
     if result.returncode != 0:
-        print(f"Error running Lighthouse command for {url}: {result.stderr.decode()}")
+        if result.returncode == None:
+            print(f"{error_message_preffix} no error code was generated")
+        else:
+            print(f"{error_message_preffix} {result.stderr.decode()}")
         return None
     else:
         try:
